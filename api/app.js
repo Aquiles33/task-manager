97ac3633df27de479ecde61422373e1e1f89e728
Aquiles Silva <aquiles33.silva@gmail.com>
2020-11-21 20:29:58 -0300

    rotas criadas

--- api/app.js
+++ api/app.js
@@ -1,8 +1,83 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+const mongoose = require('./db/mongoose')
+
+const bodyParser = require('body-parser')
+
+const { List, Task } =  require('./db/models')
+
+app.use(bodyParser.json())
+
+app.get('/lists', (req, res) => {
+  List.find({}).then((lists) => {
+    res.send(lists)
+  })
+})
+
+app.post('/lists', (req, res) => {
+  let title = req.body.title;
+  let newList = new List({
+    title
+  });
+  newList.save().then((listDoc) => {
+    res.send(listDoc)
+  })
+})
+
+app.patch('/lists/:id', (req, res) => {
+  List.findOneAndUpdate({ _id: req.params.id }, {
+    $set: req.body
+  }).then(() => {
+    res.sendStatus(200)
+  })
+})
+
+app.delete('/lists/:id', (req, res) => {
+  List.findOneAndRemove({ _id: req.params.id }).then((removedListDoc) => {
+    res.send(removedListDoc)
+  })
+})
+
+app.get('/lists/:listId/tasks', (req, res) => {
+  Task.find({
+    _listId: req.params.listId
+  }).then((tasks) => {
+    res.send(tasks)
+  })
+})
+
+// ============================== //
+
+app.post('/lists/:listId/tasks', (req, res) => {
+  let newTask = new Task({
+    title: req.body.title,
+    _listId: req.params.listId    
+  })
+  newTask.save().then((newTaskDoc) => {
+    res.send(newTaskDoc)
+  })
+})
+
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+  Task.findOneAndUpdate({
+    _id: req.params.taskId,
+    _listId: req.params.listId
+  }, {
+    $set: req.body
+  }).then(() => {
+    res.sendStatus(200)
+  })
+})
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+  Task.findOneAndRemove({
+    _id: req.params.taskId,
+    _listId: req.params.listId
+  }).then((removedTaskDoc) => {
+    res.send(removedTaskDoc)
+  })
 })
 
 app.listen(3000, () => {
