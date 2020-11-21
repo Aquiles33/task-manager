0e6c59c915e477413207bb1a4730437758577fc4
Aquiles Silva <aquiles33.silva@gmail.com>
2020-11-21 16:40:13 -0300

    front concluído

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
