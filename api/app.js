fa3630dd7b468ea651105a49e3348738f44b709f
Aquiles Silva <aquiles33.silva@gmail.com>
2020-11-22 20:10:53 -0300

    corrgindo bug no cors e adicionando propriedade no model de task

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -73,7 +74,7 @@ app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
   }, {
     $set: req.body
   }).then(() => {
-    res.sendStatus(200)
+    res.send({ message: 'Update successfully!'})
   })
 })
 
