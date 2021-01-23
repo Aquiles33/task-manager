90e23b7c67fd44d02822df7aa6b87a9bd3621dee
Aquiles Silva <aquiles33.silva@gmail.com>
2021-01-22 22:32:58 -0300

    :rocket: Métodos de login e senha concluídos

--- api/app.js
+++ api/app.js
@@ -1,92 +1,153 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-const mongoose = require('./db/mongoose')
+const mongoose = require('./db/mongoose');
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const { List, Task } =  require('./db/models')
+const { List, Task, User } = require('./db/models');
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
 app.get('/lists', (req, res) => {
   List.find({}).then((lists) => {
-    res.send(lists)
-  })
-})
+    res.send(lists);
+  });
+});
 
 app.post('/lists', (req, res) => {
   let title = req.body.title;
   let newList = new List({
-    title
+    title,
   });
   newList.save().then((listDoc) => {
-    res.send(listDoc)
-  })
-})
+    res.send(listDoc);
+  });
+});
 
 app.patch('/lists/:id', (req, res) => {
-  List.findOneAndUpdate({ _id: req.params.id }, {
-    $set: req.body
-  }).then(() => {
-    res.sendStatus(200)
-  })
-})
+  List.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
 
 app.delete('/lists/:id', (req, res) => {
   List.findOneAndRemove({ _id: req.params.id }).then((removedListDoc) => {
-    res.send(removedListDoc)
-  })
-})
+    res.send(removedListDoc);
+  });
+});
 
 app.get('/lists/:listId/tasks', (req, res) => {
   Task.find({
-    _listId: req.params.listId
+    _listId: req.params.listId,
   }).then((tasks) => {
-    res.send(tasks)
-  })
-})
+    res.send(tasks);
+  });
+});
 
 // ============================== //
 
 app.post('/lists/:listId/tasks', (req, res) => {
   let newTask = new Task({
     title: req.body.title,
-    _listId: req.params.listId    
-  })
+    _listId: req.params.listId,
+  });
   newTask.save().then((newTaskDoc) => {
-    res.send(newTaskDoc)
-  })
-})
-
+    res.send(newTaskDoc);
+  });
+});
 
 app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
-  Task.findOneAndUpdate({
-    _id: req.params.taskId,
-    _listId: req.params.listId
-  }, {
-    $set: req.body
-  }).then(() => {
-    res.send({ message: 'Update successfully!'})
-  })
-})
+  Task.findOneAndUpdate(
+    {
+      _id: req.params.taskId,
+      _listId: req.params.listId,
+    },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.send({ message: 'Update successfully!' });
+  });
+});
 
 app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
   Task.findOneAndRemove({
     _id: req.params.taskId,
-    _listId: req.params.listId
+    _listId: req.params.listId,
   }).then((removedTaskDoc) => {
-    res.send(removedTaskDoc)
-  })
-})
+    res.send(removedTaskDoc);
+  });
+});
+
+app.post('/users', (req, res) => {
+  let body = req.body;
+  let newUser = new User(body);
+
+  newUser
+    .save()
+    .then(() => {
+      return newUser.createSession();
+    })
+    .then((refreshToken) => {
+      return newUser.generateAccessAuthToken().then((accessToken) => {
+        return { accessToken, refreshToken };
+      });
+    })
+    .then((authTokens) => {
+      res
+        .header('x-refresh-token', authTokens.refreshToken)
+        .header('x-access-token', authTokens.accessToken)
+        .send(newUser);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.post('/users/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  User.findByCredentials(email, password)
+    .then((user) => {
+      return user
+        .createSession()
+        .then((refreshToken) => {
+          return user.gerenateAccessAuthToken().then((accessToken) => {
+            return { accessToken, refreshToken };
+          });
+        })
+        .then((authTokens) => {
+          res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(newUser);
+        });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
-})
+});
